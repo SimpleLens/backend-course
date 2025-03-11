440862a51a4272e53948111817c53103b6a1ff05
Lens <let4ik228guuu@gmail.com>
2025-03-11 15:58:40 +0700

    3 дз, миграция для rooms

--- src/config.py
+++ src/config.py
@@ -6,12 +6,12 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
-    DB_NAME: str
+    DB_NAME: str    
 
     @property
     def DB_URL(self):
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PASS}/{self.DB_NAME}'
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(env_file=Path(__file__).parent.parent/".env")
 
-settings = Settings
+settings = Settings()
